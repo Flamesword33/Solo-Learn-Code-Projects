863593de8130b3ec15e9d452bb3a57ba7f8170b2
Nathan Pelletier <npelleti@ualberta.ca>
2022-10-26 22:42:25 -0600

    Create 19 Loan Calculator.java

---
+++ Java/19 Loan Calculator.java
@@ -0,0 +1,33 @@
+/** 19 Loan Calculator.java
+ * by Nathan Pelletier 
+ * October 26, 2022
+ *
+ * You take a loan from a friend and need to calculate how much you will owe him after 3
+ * months. You are going to pay him back 10% of the remaining loan amount each month.
+ * 
+ * Create a program that takes the loan amount as input, calculates and outputs the
+ * remaining amount after 3 months.
+ * 
+ * Sample Input:
+ * 20 000
+ * 
+ * Sample Output:
+ * 14 580
+ * 
+ * Here is the monthly payment schedule:
+ * Month 1
+ * Payment: 10% of 20 000 = 2000
+ * Remaining amount: 18 000
+ * Month 2
+ * Payment: 1800
+ * Remaining amount: 16200
+ * Month 3
+ * payment: 1620
+ * remaining amount: 14580 
+ */
+ 
+
+public class LoanCalculator{
+  public static void main(String[] args){
+  }//main
+}//LoanCalculator
